67fa86c2a810ecf6c78456eaeca315a8b0176759
paul serafim <paul.serafim.3021@gmail.com>
2022-11-03 20:44:39 +1100

    homescreen, nav

---
+++ src/HomeScreen.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from './Banner'
+import "./HomeScreen.css"
+import Nav from './Nav'
+
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+        <Nav />
+        
+        <Banner />
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyByiFw1hFRwGx1tAodmuw3GzwcA8RRJs10",
+    authDomain: "netflix-build-2-a522a.firebaseapp.com",
+    projectId: "netflix-build-2-a522a",
+    storageBucket: "netflix-build-2-a522a.appspot.com",
+    messagingSenderId: "462625003138",
+    appId: "1:462625003138:web:6878bb8722af654676469b"
+  };
