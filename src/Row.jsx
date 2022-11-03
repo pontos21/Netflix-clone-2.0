0b5240f7e4f958367af8bc01e006e75701955eaa
paul serafim <paul.serafim.3021@gmail.com>
2022-11-04 10:46:02 +1100

    banner, row & firebase deploy

--- src/Row.jsx
+++ src/Row.jsx
@@ -19,9 +19,21 @@ function Row({title, fetchUrl, isLargeRow = false}) {
   return (
     <div className='row'>
         <h2>{title}</h2>
+
         <div className="row__posters">
-           {movies.map(movie => (
-            <img className={`row__poster ${isLargeRow && 'row__posterLarge'}`} key={movie.id} src={`${base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} />
+           {movies.map(
+            (movie) => 
+            ((isLargeRow && movie.poster_path || 
+            (!isLargeRow && movie.backdrop_path)) && (
+               <img 
+               className={`row__poster ${isLargeRow && 'row__posterLarge'}`} 
+               key={movie.id} 
+               src={`${base_url}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`} 
+                alt={movie.name} 
+                /> 
+            )
         ))} 
         </div>
     </div>
