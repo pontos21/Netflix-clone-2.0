c1af65a2139a1cd62031a427661aa1b2ec67981c
paul serafim <paul.serafim.3021@gmail.com>
2022-11-04 18:52:58 +1100

    redux

--- src/App.js
+++ src/App.js
@@ -4,20 +4,28 @@ import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import LoginScreen from './screens/LoginScreen';
 import { auth } from "./firebase"
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice'
 
 
 
 function App() {
-  const user = null
+  const user = useSelector(selectUser)
+  const dispatch = useDispatch()
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         // Logged In
         console.log(userAuth)
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
       }
       else {
         // Logged Out
+        dispatch(logout)
       }
     })
     return unsubscribe;
