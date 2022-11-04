4196d825b7619ffbcb6fe19702699cc207f3a985
paul serafim <paul.serafim.3021@gmail.com>
2022-11-04 13:02:00 +1100

    login screen

---
+++ src/screens/LoginScreen.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import './LoginScreen.css'
+
+function LoginScreen() {
+  return (
+    <div className='loginScreen'>
+        <div className="loginScreen__background">
+            <img className='loginScreen__logo'
+            src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" 
+            alt="" 
+            />
+            <button className='loginScreen__button'>
+                Sign In
+            </button>
+            <div className="loginScreen__gradient" />
+        </div>
+
+        <div className="loginScreen__body">
+            <>
+                <h1>Unlimited films, Tv programmes and more.</h1>
+                <h2>Watch anywhere. Cancel at any time.</h2>
+                <h3>Ready to watch? Enter your email to create or restart your membership</h3>
+                <div className="loginScreen__input">
+                    <form>
+                        <input type="email" placeholder="Email Address" />
+                        <button className='loginScreen__getStarted'>GET STARTED</button>
+                    </form>
+                </div>
+            </>
+        </div>
+    </div>
+  )
+}
+
+export default LoginScreen
