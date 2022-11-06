bbfbb5c7a9c322afb8593dd5b607e537612b3695
paul serafim <paul.serafim.3021@gmail.com>
2022-11-06 16:23:46 +1100

    finished build

--- src/screens/PlansScreen.jsx
+++ src/screens/PlansScreen.jsx
@@ -1,92 +1,164 @@
-import React, { useState, useEffect } from 'react'
-import "./PlansScreen.css"
-import db from '../firebase'
+import React, { useEffect, useState } from "react";
+import db from "../firebase";
+import "./PlansScreen.css";
 import {
-    collection,
-    doc,
-    setDoc,
-    query,
-    where,
-    getDocs,
-    CollectionReference,
-    onSnapshot,
-    getDoc,
-    addDoc,
-  } from "firebase/firestore";
-import { useSelector } from 'react-redux';
+  collection,
+  doc,
+  setDoc,
+  query,
+  where,
+  getDocs,
+  CollectionReference,
+  onSnapshot,
+  getDoc,
+  addDoc,
+} from "firebase/firestore";
+import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
-import { loadStripe } from '@stripe/stripe-js';
-
-
+// import { loadStripe } from "@stripe/stripe-js";
 
 
 function PlansScreen() {
-    const [products, setProducts] = useState([]);
-    const user = useSelector(selectUser)
+  const [products, setProducts] = useState([]);
+  const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
 
-    useEffect(() => {
-        const q = query(collection(db, "products"), where("active", "==", true));
+  useEffect(() => {
+    const q = query(collection(db, "customers", user.uid, "subscriptions"));
 
-        onSnapshot(q, (querySnapshot) => {
-          const products = {};
-    
-          querySnapshot.forEach(async (productDoc) => {
-            products[productDoc.id] = productDoc.data();
-    
-            const productDocRef = doc(db, "products", productDoc.id);
-            
-            const priceSnap = await getDocs(collection(productDocRef, "prices"));
-    
-            priceSnap.forEach((price) => {
-              products[productDoc.id].prices = {
-                priceId: price.id,
-                priceData: price.data(),
-              };
-            });
-          });
-          setProducts(products);
+    onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach(async (subscription) => {
+        // console.log(subscription.data());
+
+        setSubscription({
+          role: subscription.data().role,
+          current_period_start:
+            subscription.data().current_period_start.seconds,
+          current_period_end: subscription.data().current_period_end.seconds,
         });
-    }, [])
+      });
+    });
+  }, [user.uid]);
 
-    const loadCheckout = async (priceId) => {
-        const docRef = await db.collection('customers')
-        .doc(user.uid).collection('checkout_sessions').add({
-            price: priceId,
-            success_url: window.location.origin,
-            cancel_url: window.location.origin,
-        })
+  useEffect(() => {
+    // OLD VERSION Firebase v8 HIDDEN HERE --- OLD VERSION Firebase v8 HIDDEN HERE
 
-        docRef.onSnapshot(async(snap) => {
-            const { error, sessionId } = snap.data()
-            if (error) {
-               alert(`An error occured: ${error.message}`) 
-            }
+    // db.collection("products").where("active", "==", true).get().then((querySnapshot) => {
+    //     const products = {};
 
-            if (sessionId) {
+    //     querySnapshot.forEach(async(productDoc) => {
+    //       products[productDoc.id] = productDoc.data();
 
-                const stripe = await loadStripe("pk_test_51M0ZESABacjKF9nZauy1WCynNUfQAKuKGZjxYQGE9cfxRD7YEsXP0cReUFgSobViGYXLybGyEOXcKLQwORsUgKKT00uJdXwj2E");
-                stripe.redirectToCheckout({ sessionId })
-            }
-        })
-    }
+    //       const priceSnap = await productDoc.ref.collection("prices").get();
+
+    //       priceSnap.docs.forEach((price) => {
+    //         products[productDoc.id].prices = {
+    //           priceId: price.id,
+    //           priceData: price.data,
+    //         };
+    //       });
+    //     });
+    //     setProducts(products);
+    //   });
+
+    //   NEW VERSION 9 --- NEW VERSION 9 --- NEW VERSION 9
+
+    const q = query(collection(db, "products"), where("active", "==", true));
+
+    onSnapshot(q, (querySnapshot) => {
+      const products = {};
+
+      querySnapshot.forEach(async (productDoc) => {
+        products[productDoc.id] = productDoc.data();
+
+        const productDocRef = doc(db, "products", productDoc.id);
+        // REFERENCE NA productDoc zde musi byt a tu potom použit v getDoc collection
+        const priceSnap = await getDocs(collection(productDocRef, "prices"));
+
+        priceSnap.forEach((price) => {
+          products[productDoc.id].prices = {
+            priceId: price.id,
+            priceData: price.data(),
+          };
+        });
+      });
+      setProducts(products);
+    });
+  }, []);
+
+//   console.log(products);
+//   console.log(subscription);
+  
+
+  const loadCheckout = async (priceId) => {
+    const docRef = await addDoc(
+      collection(db, "customers", user.uid, "checkout_sessions"),
+      {
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      }
+    );
+
+    onSnapshot(docRef, async (snap) => {
+      const { error, sessionId } = snap.data();
+
+      if (error) {
+        // Show an error to a customer and inspect your
+        // Cloud functions logs in the firebase console.
+        alert(`An error occurred: ${error.message}`);
+      }
+      if (sessionId) {
+        // We have a session, let's redirect to Checkout
+        // Init Stripe
+        // const stripe = await loadStripe(
+        //   "pk_test_51M0ZESABacjKF9nZauy1WCynNUfQAKuKGZjxYQGE9cfxRD7YEsXP0cReUFgSobViGYXLybGyEOXcKLQwORsUgKKT00uJdXwj2E"
+        // );
+        // stripe.redirectToCheckout({ sessionId });
+      }
+    });
+  };
 
   return (
-    <div className='plansScreen'>
-        {Object.entries(products).map(([productId, productData]) => {
-            // add some logic to check if users subscription is active
-            return (
-                <div className="plansScreen__plan">
-                    <div className="plansScreen__info">
-                        <h5>{productData.name}</h5>
-                        <h6>{productData.description}</h6>
-                    </div>
+    <div className="plansScreen">
+        <br />
+      {subscription && (
+        <p className="plansScreen__renewal">
+          Renewal date:{" "}
+          {new Date(subscription?.current_period_end * 1000).toLocaleDateString(
+            "CS-cs"
+          )}
+        </p>
+      )}
+      {Object.entries(products).map(([productId, productData]) => {
+        // TODO: Add some logic to check if the user's subscription is active
+        const isCurrentPackage = productData.name
+          ?.toLowerCase()
+          .includes(subscription?.role);
 
-                    <button onClick={() => loadCheckout(productData?.prices?.priceId)}>Subscribe</button>
-                </div>
-            )
-        })}
+        return (
+          <div
+            key={productId}
+            className={`${
+              isCurrentPackage && "plansScreen__plan--disabled"
+            } plansScreen__plan`}
+          >
+            <div className="plansScreen__info">
+              <h5>{productData.name}</h5>
+              <h6>{productData.description}</h6>
+            </div>
+            <button
+              onClick={() =>
+                !isCurrentPackage && loadCheckout(productData.prices.priceId)
+              }
+            >
+              {isCurrentPackage ? "Current Plan" : "Subscribe"}
+            </button>
+          </div>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default PlansScreen
+export default PlansScreen;
--- src/screens/ProfileScreen.jsx
+++ src/screens/ProfileScreen.jsx
@@ -1,35 +1,63 @@
-import React from 'react'
-import "./ProfileScreen.css"
-import Nav from "../Nav"
-import { selectUser  } from "../features/userSlice"
-import { useSelector } from 'react-redux'
-import { auth } from '../firebase'
-import PlansScreen from './PlansScreen'
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "../features/userSlice";
+import db, { auth } from "../firebase";
+import Nav from "../Nav";
+import PlansScreen from "./PlansScreen";
+import "./ProfileScreen.css";
+import { query, collection, where, onSnapshot } from "firebase/firestore";
 
 function ProfileScreen() {
-    const user = useSelector(selectUser)
-  return (
-    <div className='profileScreen'>
-        <Nav />
-        <div className="profileScreen__body">
-            <h1>Edit Profile</h1>
-            <div className="profileScreen__info">
-                <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" 
-                />
-                <div className="profileScreen__details">
-                    <h2>{user.email}</h2>
-                    <div className="profileScreen__plans">
-                        <h3>Plans</h3>
+  const user = useSelector(selectUser);
+  const [subscription, setSubscription] = useState(null);
 
-                        <PlansScreen />
-                        <button onClick={() => auth.signOut()} 
-                        className='profileScreen__signOut'>Sign Out</button>
-                    </div>
-                </div>
+  useEffect(() => {
+    const q = query(collection(db, "customers", user.uid, "subscriptions"));
+
+    onSnapshot(q, (querySnapshot) => {
+      querySnapshot.forEach(async (subscription) => {
+        // console.log(subscription.data());
+
+        setSubscription(subscription.data().role)
+        });
+      });
+    
+  }, [user.uid]);
+
+  return (
+    <div className="profileScreen">
+      <Nav />
+      <div className="profileScreen__body">
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+            alt=""
+          />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <div className="profileScreen__plansHeading">
+              <h3> {`Plans`} </h3>
+              {subscription ? (
+                <p>{`(Current: ${subscription})`}</p>
+              ) : <p>{`Not subscribed`}</p>}
+              
+              </div>
+
+              <PlansScreen />
+              <button
+                onClick={() => auth.signOut()}
+                className="profileScreen__signOut"
+              >
+                Sign Out
+              </button>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ProfileScreen
+export default ProfileScreen;
